dfe265c3d0620a117b0692120e30f619706828cd
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-04-12 18:37:19 +0300

    solve kui_4/_01

---
+++ src/codewars/kui_4/_01/task.test.ts
@@ -0,0 +1,47 @@
+import { sumIntervals } from "./task";
+
+describe("task_01", () => {
+  const data: Array<{ intervals: Array<[number, number]>; expected: number }> =
+    [
+      { intervals: [[1, 5]], expected: 4 },
+      {
+        intervals: [
+          [1, 5],
+          [6, 10],
+        ],
+        expected: 8,
+      },
+      {
+        intervals: [
+          [1, 5],
+          [1, 5],
+        ],
+        expected: 4,
+      },
+      {
+        intervals: [
+          [1, 4],
+          [7, 10],
+          [3, 5],
+        ],
+        expected: 7,
+      },
+      {
+        intervals: [[-1e9, 1e9]],
+        expected: 2e9,
+      },
+      {
+        intervals: [
+          [0, 20],
+          [-1e8, 10],
+          [30, 40],
+        ],
+        expected: 1e8 + 30,
+      },
+    ];
+
+  test("the solution is true", () => {});
+  test.each(data)(`the solution is true`, ({ intervals, expected }) => {
+    expect(sumIntervals(intervals)).toBe(expected);
+  });
+});
