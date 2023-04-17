b91cfcdfa20ffef6e2671d712b574512fd18c6dc
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-04-17 15:04:22 +0300

    solve leetcode/_1431

---
+++ src/leetcode/_1431/task.test.ts
@@ -0,0 +1,32 @@
+import { kidsWithCandies } from "./task";
+
+describe("task_01", () => {
+  const data: Array<{
+    candies: Array<number>;
+    extraCandies: number;
+    expected: Array<boolean>;
+  }> = [
+    {
+      candies: [2, 3, 5, 1, 3],
+      extraCandies: 3,
+      expected: [true, true, true, false, true],
+    },
+    {
+      candies: [4, 2, 1, 1, 2],
+      extraCandies: 1,
+      expected: [true, false, false, false, false],
+    },
+    {
+      candies: [12, 1, 12],
+      extraCandies: 10,
+      expected: [true, false, true],
+    },
+  ];
+
+  test.each(data)(
+    `$#. the solution is true`,
+    ({ candies, extraCandies, expected }) => {
+      expect(kidsWithCandies(candies, extraCandies)).toEqual(expected);
+    }
+  );
+});
